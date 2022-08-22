47cbe1d5a73273d6842de8128e0cc62c7b8382aa
rzfhlvb <rezafahlevibahtiar@gmail.comgit config --global user.email rezafahlevibahtiar@gmail.com>
2022-08-23 06:59:40 +0700

    v4

---
+++ v4/src/assets/items.js
@@ -0,0 +1,40 @@
+import website from "../assets/website.png";
+import instagram from "../assets/instagram.png";
+import linkedin from "../assets/linkedin.jpg";
+import github from "../assets/github.png";
+import twitter from "../assets/twitter.png";
+
+const items = [
+  {
+    title: "Website",
+    subtitle: "Look at my work!",
+    image: website,
+    link: "https://rezafahlevi.com",
+  },
+  {
+    title: "GitHub",
+    subtitle: "@rzfhlvb |  all my source code",
+    image: github,
+    link: "https://github.com/rzfhlvb",
+  },
+  {
+    title: "Instagram",
+    subtitle: "@rzfhlvb |  Shots of my boring life 🙂",
+    image: instagram,
+    link: "https://instagram.com/rzfhlvb",
+  },
+  {
+    title: "Twitter",
+    subtitle: "@rzfhlvb | 😉",
+    image: twitter,
+    link: "https://twitter.com/txtdarireza",
+  },
+  {
+    title: "LinkedIn",
+    subtitle: "Know more about my profession 👨‍💼",
+    image: linkedin,
+    link: "https://www.linkedin.com/in/rezafahlevi/",
+  },
+];
+
+export default items;
---
+++ v4/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "../css/skeleton.css";
+import "../css/normalize.css";
+import "../css/components.css";
+
+function Footer() {
+  return (
+    <div className="Footer container">
+      <p>
+        Made with <span class="heart">❤️</span> by <a href={"https://twitter.com/txtdarireza"}>Reza Fahlevi</a>
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
